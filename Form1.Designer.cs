2899741ebd32a09886684544648044ca7f6580e9
ivanpost777 <ivanpost777@gmail.com>
2019-09-27 16:31:56 +0300

    uzername

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsAppDrawBitmap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.numericUpDownScaleFactor = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownAngle = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownScaleFactor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAngle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(984, 661);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.numericUpDownScaleFactor);
+            this.flowLayoutPanel1.Controls.Add(this.numericUpDownAngle);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 632);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(978, 26);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // numericUpDownScaleFactor
+            // 
+            this.numericUpDownScaleFactor.DecimalPlaces = 4;
+            this.numericUpDownScaleFactor.Increment = new decimal(new int[] {
+            25,
+            0,
+            0,
+            131072});
+            this.numericUpDownScaleFactor.Location = new System.Drawing.Point(3, 3);
+            this.numericUpDownScaleFactor.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownScaleFactor.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownScaleFactor.Name = "numericUpDownScaleFactor";
+            this.numericUpDownScaleFactor.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownScaleFactor.TabIndex = 1;
+            this.numericUpDownScaleFactor.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownScaleFactor.ValueChanged += new System.EventHandler(this.numericUpDownScaleFactor_ValueChanged);
+            // 
+            // numericUpDownAngle
+            // 
+            this.numericUpDownAngle.DecimalPlaces = 2;
+            this.numericUpDownAngle.Location = new System.Drawing.Point(129, 3);
+            this.numericUpDownAngle.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.numericUpDownAngle.Name = "numericUpDownAngle";
+            this.numericUpDownAngle.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownAngle.TabIndex = 2;
+            this.numericUpDownAngle.ValueChanged += new System.EventHandler(this.numericUpDownAngle_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownScaleFactor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAngle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.NumericUpDown numericUpDownScaleFactor;
+        private System.Windows.Forms.NumericUpDown numericUpDownAngle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppDrawBitmap
+{
+    public partial class Form1 : Form
+    {
+        public UserControl ctrlToDraw;
+        private Bitmap bitmapInitial;
+        private Bitmap bitmapRender;
+        public double currentscalefactor = 1.0; public double scaledelta = 0.1;
+        public double currentanglevalue = 0.0;
+        bool triggerscaleChangedEvt = true; bool triggerrotationChangedEvt = true;
+        public Form1()
+        {
+            InitializeComponent();
+            ctrlToDraw = new UserControl();
+            ctrlToDraw.Dock = DockStyle.Fill;
+            ctrlToDraw.BackColor = Color.White;
+            ctrlToDraw.Paint += CtrlToDraw_Paint;
+            tableLayoutPanel1.Controls.Add(ctrlToDraw, 0, 0);
+            InitializeBitmaps();
+            triggerscaleChangedEvt = false;
+            numericUpDownScaleFactor.Value = (decimal)currentscalefactor;
+            triggerscaleChangedEvt = true;
+            ctrlToDraw.Refresh();
+        }
+
+        private void InitializeBitmaps()
+        {
+            bitmapInitial = new Bitmap(100, 100);
+            Pen blackPen = new Pen(Color.Black,2.0f);
+            Graphics g = Graphics.FromImage(bitmapInitial);
+            g.DrawArc(blackPen, 5, 5, 20, 20, 45, 90);
+            g.DrawLine(blackPen, 40, 0, 30, 100);
+            g.DrawLine(blackPen, 10, 2, 90, 2);
+            g.DrawLine(blackPen, 90, 2, 90, 95);
+            g.DrawLine(blackPen, 10, 95, 90, 95);
+            g.DrawLine(blackPen, 10, 95, 10, 2);
+
+            bitmapRender = new Bitmap(bitmapInitial);            
+        }
+
+        private void CtrlToDraw_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bitmapRender, new Point(0, 0));
+        }
+        public double ConvertDegreesToRadians(double degrees)
+        {
+            double radians = (Math.PI / 180) * degrees;
+            return (radians);
+        }
+        public double ConvertRadiansToDegrees(double radians)
+        {
+            return radians * (180.0 / Math.PI);
+        }
+        // https://stackoverflow.com/questions/2163829/how-do-i-rotate-a-picture-in-winforms
+        private void rescaleImageUsingCurrentScaleFactor() {
+            double newWidthPrescaled = currentscalefactor * bitmapInitial.Width;
+            double newHeightPrescaled = currentscalefactor * bitmapInitial.Height;
+           
+            bitmapRender = new Bitmap((int)(newWidthPrescaled),(int)(newHeightPrescaled));
+            using (Graphics ee = Graphics.FromImage(bitmapRender))
+            {
+                ee.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                ee.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                ee.DrawImage(bitmapInitial, 0, 0, (float)(newWidthPrescaled), (float)(newHeightPrescaled));
+
+            }                                        
+        }
+
+        private Bitmap RotateImageUsingCurrentRotationAngle()
+        {
+            //First, re-center the image in a larger image that has a margin/frame
+            //to compensate for the rotated image's increased size
+            float angle = (float) currentanglevalue;
+            Bitmap rotateMe = bitmapRender;
+            double ALPHA_DEG = currentanglevalue;
+            double BETA_DEG = 90 - currentanglevalue;
+            double ALPHA_RAD = ConvertDegreesToRadians(ALPHA_DEG);
+            double BETA_RAD = ConvertDegreesToRadians(BETA_DEG);
+            double newHeight = Math.Abs(Math.Abs(rotateMe.Height * Math.Sin(ALPHA_RAD)) + Math.Abs(rotateMe.Width * Math.Sin(BETA_RAD)));
+            double newWidth = Math.Abs(Math.Abs(rotateMe.Height * Math.Cos(ALPHA_RAD)) + Math.Abs(rotateMe.Width * Math.Cos(BETA_RAD)));
+            double offsetHeight = (newHeight - rotateMe.Height) / 2.0;
+            double offsetWidth = (newWidth -  rotateMe.Width) / 2.0;
+            System.Diagnostics.Debug.Assert(offsetHeight >= 0);
+            System.Diagnostics.Debug.Assert(offsetWidth >= 0);
+            var bmp = new Bitmap((int)newWidth, (int)newHeight);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+                g.DrawImageUnscaled(rotateMe, (int)offsetWidth, (int)offsetHeight, bmp.Width, bmp.Height);
+
+            rotateMe = bmp;
+
+            //Now, actually rotate the image
+            Bitmap rotatedImage = new Bitmap(rotateMe.Width, rotateMe.Height);
+
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                g.TranslateTransform(rotateMe.Width / 2, rotateMe.Height / 2);   //set the rotation point as the center into the matrix
+                g.RotateTransform(angle);                                        //rotate
+                g.TranslateTransform(-rotateMe.Width / 2, -rotateMe.Height / 2); //restore rotation point into the matrix
+                g.DrawImage(rotateMe, new Point(0, 0));                          //draw the image on the new bitmap
+            }
+
+            return rotatedImage;
+        }
+
+        private void numericUpDownScaleFactor_ValueChanged(object sender, EventArgs e)
+        {
+            if (triggerscaleChangedEvt == false) return;
+            this.currentscalefactor = (double) numericUpDownScaleFactor.Value;
+            rescaleImageUsingCurrentScaleFactor();
+            this.bitmapRender = RotateImageUsingCurrentRotationAngle();
+            ctrlToDraw.Refresh();
+        }
+
+        private void numericUpDownAngle_ValueChanged(object sender, EventArgs e)
+        {
+            if (triggerrotationChangedEvt == false) return;
+            this.currentanglevalue = (double)numericUpDownAngle.Value;
+            rescaleImageUsingCurrentScaleFactor();
+            this.bitmapRender = RotateImageUsingCurrentRotationAngle();
+            ctrlToDraw.Refresh();
+        }
+    }
+}
